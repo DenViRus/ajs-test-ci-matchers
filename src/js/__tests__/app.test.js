bcc8c183b0b885edb09c23786939ca279f7a5797
Denis Yefremenko <denvirus83@mail.ru>
2021-08-31 20:09:10 +0400

    test-ci, pure-functions: tests added

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -9,3 +9,73 @@ test('should check health = 60', () => {
 
   expect(received).toBe(expected);
 });
+
+test('should check health = 50', () => {
+  const testHero = { name: 'Маг', health: 50 };
+
+  const expected = 'healthy';
+
+  const received = appGetHealth(testHero);
+
+  expect(received).toBe(expected);
+});
+
+test('should check health = 35', () => {
+  const testHero = { name: 'Маг', health: 35 };
+
+  const expected = 'wounded';
+
+  const received = appGetHealth(testHero);
+
+  expect(received).toBe(expected);
+});
+
+test('should check health = 15', () => {
+  const testHero = { name: 'Маг', health: 15 };
+
+  const expected = 'wounded';
+
+  const received = appGetHealth(testHero);
+
+  expect(received).toBe(expected);
+});
+
+test('should check health = 10', () => {
+  const testHero = { name: 'Маг', health: 10 };
+
+  const expected = 'critical';
+
+  const received = appGetHealth(testHero);
+
+  expect(received).toBe(expected);
+});
+
+test('should check health = 1', () => {
+  const testHero = { name: 'Маг', health: 1 };
+
+  const expected = 'critical';
+
+  const received = appGetHealth(testHero);
+
+  expect(received).toBe(expected);
+});
+
+test('should check health = 0', () => {
+  const testHero = { name: 'Маг', health: 0 };
+
+  const expected = 'dead';
+
+  const received = appGetHealth(testHero);
+
+  expect(received).toBe(expected);
+});
+
+test('should check health = -10', () => {
+  const testHero = { name: 'Маг', health: -10 };
+
+  const expected = 'dead';
+
+  const received = appGetHealth(testHero);
+
+  expect(received).toBe(expected);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,9 @@
 export function getHealth(hero) {
   if (hero.health >= 50) {
     return 'healthy';
-  } if (hero.health < 50 >= 15) {
+  } if (hero.health < 50 && hero.health >= 15) {
     return 'wounded';
-  } if (hero.health < 15 >= 1) {
+  } if (hero.health < 15 && hero.health >= 1) {
     return 'critical';
   }
   return 'dead';
