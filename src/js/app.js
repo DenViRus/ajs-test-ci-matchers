4f998102a03f3bef13b7d371007dc4d083c9d849
Denis Yefremenko <denvirus83@mail.ru>
2021-09-01 11:05:21 +0400

    test-ci, matchers initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,1 @@
 // TODO: write your code here
-export function getHealth(hero) {
-  if (hero.health >= 50) {
-    return 'healthy';
-  } if (hero.health < 50 && hero.health >= 15) {
-    return 'wounded';
-  } if (hero.health < 15 && hero.health >= 1) {
-    return 'critical';
-  }
-  return 'dead';
-}
