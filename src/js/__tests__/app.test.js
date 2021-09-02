7c717841d7e93eed106dc48c9ac2573c654c8765
Denis Yefremenko <denvirus83@mail.ru>
2021-09-02 11:13:38 +0400

    test-ci, matchers: toBe added, toStringEqual added

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,5 +1,79 @@
 import { sortByHealth as appSortByHealth } from '../app.js';
 
+test('should check array with deadman (JSON.stringify)', () => {
+  const friends = [
+    { name: 'маг', health: 0 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  const received = appSortByHealth(friends);
+
+  const expected = [
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 0 },
+  ];
+  expect(JSON.stringify(received)).toBe(JSON.stringify(expected));
+});
+
+test('should check big array (JSON.stringify)', () => {
+  const friends = [
+    { name: 'лучник', health: 80 },
+    { name: 'маг', health: 120 },
+    { name: 'кавалерист', health: 200 },
+    { name: 'мечник', health: 10 },
+    { name: 'рыцарь', health: 150 },
+    { name: 'арбалетчик', health: 50 },
+  ];
+
+  const received = appSortByHealth(friends);
+
+  const expected = [
+    { name: 'кавалерист', health: 200 },
+    { name: 'рыцарь', health: 150 },
+    { name: 'маг', health: 120 },
+    { name: 'лучник', health: 80 },
+    { name: 'арбалетчик', health: 50 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(JSON.stringify(received)).toBe(JSON.stringify(expected));
+});
+
+test('should check sort array (toBe)', () => {
+  const friends = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  const received = appSortByHealth(friends);
+
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(received.toString()).toBe(expected.toString());
+});
+
+test('should check sort reverse array (toBe)', () => {
+  const friends = [
+    { name: 'мечник', health: 10 },
+    { name: 'лучник', health: 80 },
+    { name: 'маг', health: 100 },
+  ];
+
+  const received = appSortByHealth(friends);
+
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(received.toString()).toBe(expected.toString());
+});
+
 test('should check sort array', () => {
   const friends = [
     { name: 'маг', health: 100 },
@@ -118,4 +192,50 @@ test('should check array with same', () => {
     { name: 'арбалетчик', health: 100 },
   ];
   expect(received).toEqual(expected);
+});
+
+test('should check array with repeated (toStrictEqual)', () => {
+  const friends = [
+    { name: 'лучник', health: 100 },
+    { name: 'маг', health: 120 },
+    { name: 'кавалерист', health: 200 },
+    { name: 'мечник', health: 100 },
+    { name: 'рыцарь', health: 150 },
+    { name: 'арбалетчик', health: 100 },
+  ];
+
+  const received = appSortByHealth(friends);
+
+  const expected = [
+    { name: 'кавалерист', health: 200 },
+    { name: 'рыцарь', health: 150 },
+    { name: 'маг', health: 120 },
+    { name: 'лучник', health: 100 },
+    { name: 'мечник', health: 100 },
+    { name: 'арбалетчик', health: 100 },
+  ];
+  expect(received).toStrictEqual(expected);
+});
+
+test('should check array with same (toStrictEqual)', () => {
+  const friends = [
+    { name: 'лучник', health: 100 },
+    { name: 'маг', health: 100 },
+    { name: 'кавалерист', health: 100 },
+    { name: 'мечник', health: 100 },
+    { name: 'рыцарь', health: 100 },
+    { name: 'арбалетчик', health: 100 },
+  ];
+
+  const received = appSortByHealth(friends);
+
+  const expected = [
+    { name: 'лучник', health: 100 },
+    { name: 'маг', health: 100 },
+    { name: 'кавалерист', health: 100 },
+    { name: 'мечник', health: 100 },
+    { name: 'рыцарь', health: 100 },
+    { name: 'арбалетчик', health: 100 },
+  ];
+  expect(received).toStrictEqual(expected);
 });
